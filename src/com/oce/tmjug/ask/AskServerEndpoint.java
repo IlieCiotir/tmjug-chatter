3fe2b0d361181cb376c5492c4ef2191b799a414f
unknown <dipixelhunter@gmail.com>
2014-07-21 19:13:52 +0300

    Working echo.

---
+++ src/com/oce/tmjug/ask/AskServerEndpoint.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package com.oce.tmjug.ask;
+
+import java.io.IOException;
+
+import javax.websocket.CloseReason;
+import javax.websocket.EndpointConfig;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+/**
+ * @author Sile
+ * 
+ */
+@ServerEndpoint(value = "/ask")
+public class AskServerEndpoint {
+	@OnOpen
+	public void onOpen(Session session, EndpointConfig config) {
+
+	}
+
+	@OnMessage
+	public void onMessage(Session session, final String message) {
+		for (Session t : session.getOpenSessions()) {
+
+			try {
+				if (t.isOpen()) {
+					t.getBasicRemote().sendText(message);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@OnError
+	public void onError(Session session, Throwable error) {
+
+	}
+
+	@OnClose
+	public void onClose(Session session, CloseReason reason) {
+
+	}
+}
